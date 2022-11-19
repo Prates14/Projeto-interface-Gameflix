11cb2be8a7816ecff8a22556399bbb9019c1b35a
Guilherme Prates <113528024+Prates14@users.noreply.github.com>
2022-11-19 16:45:46 -0300

    Add files via upload

---
+++ netflix-clone-master/js/index.js
@@ -0,0 +1,12 @@
+const url1 = "https://overwatch.blizzard.com/pt-br/";
+const url2 = "https://www.youtube.com/watch?v=PWe0pC2akpo&t=36s";
+
+function jogo() {
+    const win = window.open(url1, '_blank');
+    win.focus();
+};
+
+function trailer() {
+    const win = window.open(url2, '_blank');
+    win.focus();
+};
